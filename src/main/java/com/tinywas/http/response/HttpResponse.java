33273c9629ab3557a1a7e9894aa9416e1d7b6fcc
xxzeroeight <xxzeroeight@naver.com>
2026-06-16 17:45:59 +0900

    feat: HttpResponse, HttpResponseWriter 구현

---
+++ src/main/java/com/tinywas/http/response/HttpResponse.java
@@ -0,0 +1,62 @@
+package com.tinywas.http.response;
+
+import com.tinywas.http.HttpVersion;
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class HttpResponse {
+    private final HttpVersion version;
+    private final HttpStatus status;
+    private final Map<String, String> headers;
+    private final String body;
+
+    private HttpResponse(Builder builder) {
+        this.version = builder.version;
+        this.status = builder.status;
+        this.headers = Collections.unmodifiableMap(builder.headers);
+        this.body = builder.body;
+    }
+
+    public HttpVersion getVersion() { return version; }
+    public HttpStatus getStatus() { return status; }
+    public Map<String, String> getHeaders() { return headers; }
+    public String getBody() { return body; }
+
+    public static Builder builder(HttpStatus status) {
+        return new Builder(status);
+    }
+
+    public static class Builder {
+        private HttpVersion version = HttpVersion.HTTP_1_1;
+        private final HttpStatus status;
+        private final Map<String, String> headers = new LinkedHashMap<>();
+        private String body = "";
+
+        private Builder(HttpStatus status) {
+            this.status = status;
+        }
+
+        public Builder version(HttpVersion version) {
+            this.version = version;
+            return this;
+        }
+
+        public Builder header(String key, String value) {
+            this.headers.put(key.toLowerCase(), value);
+            return this;
+        }
+
+        public Builder body(String body) {
+            this.body = body;
+            this.headers.put("content-length", String.valueOf(body.length()));
+            return this;
+        }
+
+        public HttpResponse build() {
+            headers.putIfAbsent("connection", "close");
+            return new HttpResponse(this);
+        }
+    }
+}
